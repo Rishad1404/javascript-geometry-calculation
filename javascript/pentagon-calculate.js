5e09f1421d8a08eb329676e0f755859e49dc045a
Rishad1404 <rishadislam2019@gmail.com>
2024-02-14 18:41:34 +0600

    Pentagon Area

---
+++ javascript/pentagon-calculate.js
@@ -0,0 +1,22 @@
+function calculatePentagonArea(){
+    
+    // Get the perimeter
+    const pentagonPerimeter=document.getElementById('pentagon-size1');
+    const pentagonPerimeterText=pentagonPerimeter.value;
+    const perimeter=parseFloat(pentagonPerimeterText);
+    console.log(perimeter)
+
+    // Get the base
+    const pentagonBase=document.getElementById('pentagon-size2');
+    const pentagonBaseText=pentagonBase.value;
+    const base=parseFloat(pentagonBaseText);
+    console.log(base)
+
+    // calculate the area
+    const pentagonArea=0.5*perimeter*base;
+    console.log('The area of pentagon is: ',pentagonArea);
+
+    // Display the area
+    const pentagonAreaSpan=document.getElementById('pentagon-area');
+    pentagonAreaSpan.innerText=pentagonArea;
+}
